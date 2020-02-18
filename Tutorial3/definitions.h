2134f0ec17a28f50f5cd8d3f5df3be064a836d07
Obaid51 <16-se-51@students.uettaxila.edu.pk>
2020-02-18 22:59:37 +0500

    Create definitions.h

---
+++ Tutorial3/definitions.h
@@ -0,0 +1,44 @@
+#pragma once
+#define CL_USE_DEPRECATED_OPENCL_1_2_APIS
+#include <CL2/cl.hpp>
+#include <fstream>
+
+#define EXPORT_VAR extern
+#define EXPORT_FUNC
+
+
+extern "C" {
+
+	EXPORT_VAR unsigned int* glbSpkCntNeurons;
+	EXPORT_VAR unsigned int* glbSpkNeurons;
+	EXPORT_VAR scalar* VNeurons;
+	EXPORT_VAR scalar* UNeurons;
+	EXPORT_VAR scalar* aNeurons;
+	EXPORT_VAR scalar* bNeurons;
+	EXPORT_VAR scalar* cNeurons;
+	EXPORT_VAR scalar* dNeurons;
+	EXPORT_VAR unsigned long long iT;
+	EXPORT_VAR float t;
+
+	EXPORT_FUNC void updateNeurons(float t);
+	EXPORT_FUNC void allocateMem();
+	EXPORT_FUNC void initialize();
+	EXPORT_FUNC void initKernelPrograms();
+	EXPORT_FUNC void initKernels();
+	EXPORT_FUNC void initBuffers();
+	EXPORT_FUNC void initializeOpenCL();
+	EXPORT_FUNC void stepTime();
+	EXPORT_FUNC scalar* getCurrentVNeurons();
+	EXPORT_FUNC void pullCurrentVNeuronsFromDevice();
+
+}
+
+
+
+namespace opencl {
+
+	void setUpContext(cl::Context& context, cl::Device& device, const int deviceIndex);
+	void createProgram(const std::string& filename, cl::Program& program, cl::Context& context);
+	std::string getCLError(cl_int errorCode);
+
+}
